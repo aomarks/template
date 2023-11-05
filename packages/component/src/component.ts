c9e242f29060031552a9eec68f04538ab9f022a9
Alexander Marks <aomarks@gmail.com>
2023-11-05 08:53:43 -0800

    Format

--- packages/component/src/component.ts
+++ packages/component/src/component.ts
@@ -4,18 +4,18 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { LitElement, html, css } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import {LitElement, html, css} from 'lit';
+import {customElement} from 'lit/decorators.js';
 
 @customElement('my-element')
 export class MyComponent extends LitElement {
-    static override styles = css`
+  static override styles = css`
     :host {
       display: block;
     }
   `;
-  
-    override render() {
-        return html`<p>Hello Web</p>`;
-    }
+
+  override render() {
+    return html`<p>Hello Web</p>`;
+  }
 }
